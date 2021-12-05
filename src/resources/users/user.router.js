ca112c92d68f81b1bc5bc401544e4f4776f00ba5
Aliaksandr Filitaryn <aliaksandr.filitaryn@awtg.co.uk>
2021-12-05 22:05:33 +0300

    feat/add PUT and DELETE for USER

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -8,7 +8,34 @@ async function routes (fastify) {
       throw new Error('No users found')
     }
     return users.map(userItem => User.toResponse(userItem));
-  })
+  });
+
+  fastify.get('/users/:userId', async (request, reply) => {
+    const userId = request?.params?.userId;
+    if (userId) {
+      const user = await usersService.getById(userId);
+
+      return reply.status(200).send(User.toResponse(user));
+    }
+  
+    return null;
+  });
+
+  const deleteSchema = {
+    params: {
+      userId: { type: 'string' },
+    },
+  };
+
+  fastify.delete('/users/:userId', deleteSchema, async (request, reply) => {
+    const userId = request?.params?.userId;
+    const result = await usersService.deleteById(userId);
+    if (result) {
+      return reply.status(200).send('User deleted');
+    }
+    
+    return reply.status(404).send(new Error("User doesn't exist"));
+  });
 
   const userBodyJsonSchema = {
     type: 'object',
@@ -22,12 +49,18 @@ async function routes (fastify) {
 
   const schema = {
     body: userBodyJsonSchema,
-  }
+  };
 
-  fastify.post('/users', { schema }, async (request) => {
-    const result = await usersService.insertOne(request.body)
-    return User.toResponse(result);
-  })
+  fastify.post('/users', { schema }, async (request, reply) => {
+    const result = await usersService.insertOne(request.body);
+    return reply.status(201).send(User.toResponse(result));
+  });
+
+  fastify.put('/users/:userId', { schema }, async (request, reply) => {
+    const userId = request?.params?.userId;
+    const result = await usersService.updateOne(userId, request.body);
+    return reply.status(200).send(User.toResponse(result));
+  });
 }
 
 module.exports = routes
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -2,6 +2,24 @@ const usersRepo = require('./user.memory.repository');
 
 const getAll = () => usersRepo.getAll();
 
+const getById = (id) => usersRepo.getById(id);
+
+const deleteById = async (id) => {
+  const index = usersRepo.getIndexById(id);
+  if (index >= 0) {
+    return usersRepo.deleteByIndex(index);
+  }
+  return false;
+};
+
+const updateOne = (id, data) => {
+  const index = usersRepo.getIndexById(id);
+  if (index >= 0) {
+    return usersRepo.updateOne(id, index, data);
+  }
+  return false;
+};
+
 const insertOne = (data) => usersRepo.insertOne(data);
 
-module.exports = { getAll, insertOne };
+module.exports = { deleteById, getAll, getById, insertOne, updateOne };
