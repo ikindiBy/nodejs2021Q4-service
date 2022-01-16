ffb883902f964dd3dabdd10333cb3ced48c053b8
Aliaksandr Filitaryn <aliaksandr.filitaryn@awtg.co.uk>
2022-01-16 22:21:48 +0300

    task-8/init

--- src/resources/boards/board.model.ts
+++ src/resources/boards/board.model.ts
@@ -1,21 +1,35 @@
-import { v4 as uuid } from 'uuid';
+// import { v4 as uuid } from 'uuid';
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
 import { IBoard, IBoardAPI, IColumn } from '../../interfaces';
 
+@Entity()
 class Board implements IBoard {
-  title: string;
 
-  columns: IColumn[];
+    @PrimaryGeneratedColumn()
+    id: number;
 
-  id: string;
+    @Column()
+    title: string;
 
-  constructor({
-    title,
-    columns,
-  }: IBoardAPI) {
-    this.id = uuid();
-    this.title = title;
-    this.columns = columns || [];
-  }
+    @Column()
+    columns: IColumn[];
 }
+
+// class Board implements IBoard {
+//   title: string;
+
+//   columns: IColumn[];
+
+//   id: string;
+
+//   constructor({
+//     title,
+//     columns,
+//   }: IBoardAPI) {
+//     this.id = uuid();
+//     this.title = title;
+//     this.columns = columns || [];
+//   }
+// }
 
 export default Board;
